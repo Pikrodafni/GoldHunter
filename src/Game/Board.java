3788ea7235a5f6793fd7850d02494b098a5426a0
Orkun Alkan <44678771+Pikrodafni@users.noreply.github.com>
2020-11-05 20:49:55 +0300

    initial commit

---
+++ src/Game/Board.java
@@ -0,0 +1,55 @@
+package Game;
+
+public class Board {
+	
+	// Variables
+	// -------------------------------------------------------------
+	
+	public int rows;
+	public int columns;
+	public float gold_Rate;
+	public float secret_Gold_Rate;
+	
+	// -------------------------------------------------------------
+	// Constructor 
+	// -------------------------------------------------------------
+	public Board(int rows, int columns, float gold_Rate, float secret_Gold_Rate) {
+		super();
+		this.rows = rows;
+		this.columns = columns;
+		this.gold_Rate = gold_Rate;
+		this.secret_Gold_Rate = secret_Gold_Rate;
+	}
+	// -------------------------------------------------------------
+	// Default Values
+	// -------------------------------------------------------------
+	public Board()
+	{
+		this.rows = 20;
+		this.columns = 20;
+		this.gold_Rate = 20/100;
+		this.secret_Gold_Rate = 10/100;
+	}
+	// -------------------------------------------------------------
+	// Field of Getters and Setters
+	// -------------------------------------------------------------
+	public int getRows() {
+		return rows;
+	}
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+	public int getColumns() {
+		return columns;
+	}
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+	// -------------------------------------------------------------
+	public int[][] gold_Placement(int location[][]) {
+		
+		location[][] = null;
+		return location;
+	}
+	
+}
---
+++ src/Game/Player.java
@@ -0,0 +1,72 @@
+package Game;
+
+public class Player {
+	
+	// Variables
+	// -------------------------------------------------------------
+	public int gold;
+	public int max_Move;
+	public int move_Cost;
+	public int choosing_Target_Cost;
+	public int rows, columns;
+	// -------------------------------------------------------------
+	// Constructor
+	// -------------------------------------------------------------
+	public Player(int gold, int max_Move, int move_Cost, int choosing_Target_Cost, int rows, int columns) {
+		super();
+		this.gold = gold;
+		this.max_Move = max_Move;
+		this.move_Cost = move_Cost;
+		this.choosing_Target_Cost = choosing_Target_Cost;
+		
+	}
+	// -------------------------------------------------------------
+	// Default Values
+	// -------------------------------------------------------------
+	public Player()
+	{
+		this.gold = 200;
+		this.max_Move = 3;
+		this.move_Cost = 5;
+		this.rows = 20;
+		this.columns = 20;
+	}
+	// -------------------------------------------------------------
+	// Field of Getters and Setters
+	// -------------------------------------------------------------
+	public int getGold() {
+		return gold;
+	}
+
+	public void setGold(int gold) {
+		this.gold = gold;
+	}
+
+	public int getMax_Move() {
+		return max_Move;
+	}
+
+	public void setMax_Move(int max_Move) {
+		this.max_Move = max_Move;
+	}
+
+	public int getMove_Cost() {
+		return move_Cost;
+	}
+
+	public void setMove_Cost(int move_Cost) {
+		this.move_Cost = move_Cost;
+	}
+
+	public int getChoosing_Target_Cost() {
+		return choosing_Target_Cost;
+	}
+
+	public void setChoosing_Target_Cost(int choosing_Target_Cost) {
+		this.choosing_Target_Cost = choosing_Target_Cost;
+	}
+	
+	// -------------------------------------------------------------
+	// deneme
+	
+}
